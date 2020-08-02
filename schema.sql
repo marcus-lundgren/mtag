f428204d0ac1c578293414f13782c39951d7425c
Marcus Lundgren <marcus.lundgren@gmail.com>
2020-08-02 22:56:23 +0200

    Use the WM_CLASS property from X11 as application name.

--- schema.sql
+++ schema.sql
@@ -1,13 +1,20 @@
+CREATE TABLE application_path (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    path TEXT NOT NULL
+);
+
 CREATE TABLE application (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL
+    name TEXT NOT NULL,
+    path_id INTEGER NOT NULL,
+    FOREIGN KEY (path_id) REFERENCES application_path(id)
 );
 
 CREATE TABLE logged_entry (
-    start DATETIME NOT NULL,
-    end DATETIME NOT NULL,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     application_id INTEGER NOT NULL,
     title TEXT NOT NULL,
-    FOREIGN KEY(application_id) REFERENCES application(id)
+    start DATETIME NOT NULL,
+    end DATETIME NOT NULL,
+    FOREIGN KEY (application_id) REFERENCES application(id)
 );
-
--- watcher.py
+++ watcher.py
@@ -1,9 +1,32 @@
 import gi
 import sqlite3
+import datetime
+import subprocess
 
 gi.require_version('Wnck', '3.0')
 
 from gi.repository import Wnck
+
+print("== STARTED ==")
+
+# xprop_root_output = subprocess.run(["xprop", "-root"], stdout=subprocess.PIPE, universal_newlines=True).stdout
+# print(xprop_root_output)
+# window_id = None
+# for line in xprop_root_output.splitlines():
+#     print(line)
+#     if line.startswith("_NET_ACTIVE_WINDOW"):
+#         line_split = line.split(" ")
+#         window_id = line_split[len(line_split) - 1]
+#         print(f"Active window ID: {window_id}")
+#         break
+#
+# if window_id is None:
+#     exit(0)
+#
+# xprop_id_output = subprocess.run(["xprop", "-id", window_id], stdout=subprocess.PIPE, universal_newlines=True).stdout
+# print(xprop_id_output)
+#
+# exit(0)
 
 default_screen = Wnck.Screen.get_default()
 default_screen.force_update()
@@ -13,30 +36,51 @@ if active_window is None:
     exit(0)
 
 application = active_window.get_application()
-print(f"{application.get_name()} -> {active_window.get_name()}")
+application_pid = application.get_pid()
+application_name = active_window.get_class_group_name()
+
+print(application_pid)
+application_path = subprocess.run(["cat", f"/proc/{int(application_pid)}/cmdline"], stdout=subprocess.PIPE, universal_newlines=True).stdout
+print(application_path)
+print(f"{application_name} -> {active_window.get_name()}")
 
 db_connection = sqlite3.connect("test.db")
+db_connection.row_factory = sqlite3.Row
 db_cursor = db_connection.cursor()
 
-db_cursor.execute("SELECT id FROM application WHERE name=:name", {"name": application.get_name()})
+db_cursor.execute("SELECT id FROM application_path WHERE path=:path", {"path": str(application_path)})
+application_path_id = db_cursor.fetchone()
+if application_path_id is None:
+    print("Adding new application path")
+    db_cursor.execute("INSERT INTO application_path(path) VALUES (:path)", {"path": application_path})
+    application_path_id = db_cursor.lastrowid
+else:
+    application_path_id = application_path_id["id"]
+
+db_cursor.execute("SELECT id FROM application WHERE name=:name AND path_id=:path_id", {"name": application_name, "path_id": application_path_id})
 application_id = db_cursor.fetchone()
 if application_id is None:
     print("Adding new application")
-    db_cursor.execute("INSERT INTO application(name) VALUES (:name)", {"name": application.get_name()})
+    db_cursor.execute("INSERT INTO application(name, path_id) VALUES (:name, :path_id)", {"name": application_name, "path_id":application_path_id})
     application_id = db_cursor.lastrowid
-application_id = application_id[0]
+else:
+    application_id = application_id["id"]
+
 print(f"application_id = {application_id}")
 
 db_cursor.execute("SELECT * FROM logged_entry ORDER BY end DESC")
 last_logged_entry = db_cursor.fetchone()
+datetime_now = datetime.datetime.now()
 if last_logged_entry is None:
     print("No existing logged entry, creating a new one")
-    import datetime
-    datetime_now = datetime.datetime.now()
     db_cursor.execute("INSERT INTO logged_entry(application_id, title, start, end) VALUES (:application_id, :title, :start, :end)", {"application_id": application_id, "title": active_window.get_name(), "start": datetime_now, "end": datetime_now})
 else:
-    print(last_logged_entry)
-
+    if last_logged_entry["application_id"] == application_id:
+        print("Still same window. Update existing logged entry")
+        db_cursor.execute("UPDATE logged_entry SET end=:new_end WHERE id=:id", {"id": last_logged_entry["id"], "new_end": datetime_now})
+    else:
+        print("Not the same window. Insert new logged entry")
+        db_cursor.execute("INSERT INTO logged_entry(application_id, title, start, end) VALUES (:application_id, :title, :start, :end)", {"application_id": application_id, "title": active_window.get_name(), "start": last_logged_entry["end"], "end": datetime_now})
 
 db_connection.commit()
 db_connection.close()
