5d155e751bc50723b85afda60085e3ed19431096
Marcus Lundgren <marcus.lundgren@gmail.com>
2020-08-04 22:58:50 +0200

    Changed file name of some entities.

--- entity/__init__.py
+++ entity/__init__.py
@@ -1,4 +1,4 @@
 from entity.application import Application
-from entity.taggedentry import TaggedEntry
-from entity.loggedentry import LoggedEntry
+from entity.tagged_entry import TaggedEntry
+from entity.logged_entry import LoggedEntry
 from entity.category import Category
--- entity/logged_entry.py
+++ entity/logged_entry.py
@@ -1,7 +1,6 @@
 import datetime
 from entity.application import Application
 
-
 class LoggedEntry():
     def __init__(self, start: datetime, stop: datetime, application: Application, title: str, db_id: int = None):
         self.db_id = db_id
