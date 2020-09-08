fa2866168d7b889f01ed1684181969808e0d6b1b
Marcus Lundgren <marcus.lundgren@gmail.com>
2020-09-08 23:22:27 +0200

    Changed the path to the database file. - If the file didn't exist, run the schema script

--- mtag/helper/database_helper.py
+++ mtag/helper/database_helper.py
@@ -4,6 +4,22 @@ import os
 
 def create_connection():
     home_path = os.environ["HOME"]
-    conn = sqlite3.connect(f"{home_path}/.config/mtag.db", detect_types=sqlite3.PARSE_DECLTYPES)
+    config_path = f"{home_path}/.config/mtag/"
+    if not os.path.exists(config_path):
+        os.mkdir(config_path)
+
+    database_file_path = os.path.join(config_path, "mtag.db")
+    schema_script_needed = not os.path.exists(database_file_path)
+
+    conn = sqlite3.connect(database_file_path, detect_types=sqlite3.PARSE_DECLTYPES)
     conn.row_factory = sqlite3.Row
+
+    if schema_script_needed:
+        schema_file_path = os.path.join(os.path.dirname(__file__), "schema.sql")
+        schema_file = open(file=schema_file_path, mode="rt")
+        schema_script = schema_file.read()
+        schema_file.close()
+
+        conn.executescript(schema_script)
+
     return conn
