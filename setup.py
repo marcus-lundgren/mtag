bf592ba0442de42091a0f78dde34003f21b0c9ec
Marcus Lundgren <marcus.lundgren@gmail.com>
2020-10-02 22:40:17 +0200

    The snap for the mtag application now builds correctly.

--- setup.py
+++ setup.py
@@ -4,14 +4,15 @@ import setuptools
 from setuptools import setup
 
 setup(
-        name='mtag',
-        version='0.1.0',
-        packages=['mtag', "mtag.entity", "mtag.widget", "mtag.repository", "mtag.helper"],
-        url='',
-        license='',
-        author='ML',
-        author_email='',
-        description='',
-        python_requires='>=3.8',
-        scripts=["start_mtag"]
+    name='mtag',
+    version='0.1.0',
+    packages=['mtag', "mtag.entity", "mtag.widget", "mtag.repository", "mtag.helper"],
+    url='https://github.com/marcus-lundgren',
+    license='GPLv3',
+    author='Marcus Lundgren',
+    author_email='marcus.lundgren@gmail.com',
+    description='',
+    python_requires='>=3.6',
+    scripts=["start_mtag"],
+    package_data={"mtag.helper": ["schema.sql"]}
 )
