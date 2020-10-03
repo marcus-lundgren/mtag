8d23951c88e013d4ef3f59b2bfb75d39630d3664
Marcus Lundgren <marcus.lundgren@gmail.com>
2020-10-03 22:53:30 +0200

    Cache the to_color call in the color helper, as the output is always the same for the input

--- mtag/helper/color_helper.py
+++ mtag/helper/color_helper.py
@@ -1,6 +1,8 @@
 import hashlib
+from functools import lru_cache
 
-
+@lru_cache
 def to_color(text: str) -> str:
     hex_hash = hashlib.sha256(text.encode('utf-8')).hexdigest()
-    return f"#{hex_hash[0:6]}"
+    color_string = f"#{hex_hash[0:6]}"
+    return color_string
