8c5ab622a5d585b052de4e55bb7fee7cbe87e2e6
Marcus Lundgren <marcus.lundgren@gmail.com>
2021-03-01 21:36:52 +0100

    Made the category choice dialog more practical to use

--- mtag/widget/category_choice_dialog.py
+++ mtag/widget/category_choice_dialog.py
@@ -9,44 +9,82 @@ from gi.repository import Gtk, Gdk
 class CategoryChoiceDialog(Gtk.Dialog):
     def __init__(self, window: Gtk.Window, categories: list, tagged_entry: TaggedEntry):
         super().__init__(title="Choose category", parent=window, destroy_with_parent=True, modal=True)
+        self.set_size_request(400, 400)
         self.add_button(Gtk.STOCK_OK, Gtk.ResponseType.OK)
+        self.vbox.set_margin_start(10)
+        self.vbox.set_margin_end(10)
+        self.vbox.set_margin_top(10)
+        self.vbox.set_margin_bottom(10)
 
         time_text = datetime_helper.to_time_text(tagged_entry.start, tagged_entry.stop, tagged_entry.duration)
         time_text_label = Gtk.Label(label=time_text)
 
         self.vbox.pack_start(time_text_label, expand=False, fill=False, padding=0)
 
-        list_store = Gtk.ListStore(str)
+        self.search_box = Gtk.Entry()
+        self.search_box.connect("changed", self._do_entry_changed)
+        self.search_box.connect("key_release_event", self._do_key_pressed)
+
+        self.vbox.pack_start(self.search_box, expand=False, fill=False, padding=10)
+
+        self.list_store = Gtk.ListStore(str)
+        self.tree_model_filter: Gtk.TreeModelFilter = self.list_store.filter_new()
+        self.tree_model_filter.set_visible_func(self._filter_func)
         for c in categories:
-            list_store.append([c.name])
+            self.list_store.append([c.name])
 
-        self.combobox = Gtk.ComboBox.new_with_model_and_entry(list_store)
-        self.combobox.connect("changed", self._on_category_combobox_changed)
-        self.combobox.connect("key_release_event", self._do_key_pressed)
-        self.combobox.set_entry_text_column(0)
+        self.categories_tree_view: Gtk.TreeView = Gtk.TreeView.new_with_model(self.tree_model_filter)
+        self.categories_tree_view.show_all()
+        cat_tree_selection: Gtk.TreeSelection = self.categories_tree_view.get_selection()
+        cat_tree_selection.set_mode(Gtk.SelectionMode.BROWSE)
+        cat_tree_selection.connect("changed", self._do_selection_changed)
 
-        self.vbox.pack_start(self.combobox, expand=False, fill=False, padding=10)
+        renderer = Gtk.CellRendererText()
+        column = Gtk.TreeViewColumn("Category", renderer, text=1)
+        column.set_sort_column_id(1)
+        self.categories_tree_view.append_column(column)
+
+        swctv = Gtk.ScrolledWindow()
+        swctv.add(self.categories_tree_view)
+        swctv.set_size_request(-1, -1)
+        swctv.show_all()
+        self.vbox.pack_start(swctv, expand=True, fill=True, padding=10)
 
         total_time_title_label = Gtk.Label(label="Total previously tagged time")
         self.total_tagged_time_label = Gtk.Label(label="")
         self.vbox.pack_start(total_time_title_label, expand=False, fill=True, padding=0)
-        self.vbox.pack_start(self.total_tagged_time_label, expand=True, fill=True, padding=0)
+        self.vbox.pack_start(self.total_tagged_time_label, expand=False, fill=True, padding=0)
         self.show_all()
 
-    def _on_category_combobox_changed(self, _: Gtk.ComboBox):
-        chosen_category_name = self.get_chosen_category_value()
-        total_time = statistics_helper.get_total_category_tagged_time(chosen_category_name)
+    def get_chosen_category_value(self) -> str:
+        return self.search_box.get_text()
+
+    def _do_selection_changed(self, selection: Gtk.TreeSelection, *_):
+        _, selected = selection.get_selected()
+
+        # Ensure that we have a valid selection
+        if selected is None:
+            return
+
+        v = self.tree_model_filter.get_value(selected, 0)
+        self.search_box.set_text(v)
+
+    def _do_entry_changed(self, entry: Gtk.Entry):
+        self._update_statistics(entry.get_text())
+        self.tree_model_filter.refilter()
+
+    def _update_statistics(self, category_name: str):
+        total_time = statistics_helper.get_total_category_tagged_time(category_name)
         hours, minutes, seconds = datetime_helper.seconds_to_hour_minute_second(total_seconds=total_time)
         self.total_tagged_time_label.set_label(f"{hours} hours, {minutes} minutes, {seconds} seconds")
 
     def _do_key_pressed(self, _, e: Gdk.EventKey):
-        if e.keyval == Gdk.KEY_Return and len(self.get_chosen_category_value()) > 0:
+        if e.keyval == Gdk.KEY_Return and len(self.search_box.get_text()) > 0:
             self.response(Gtk.ResponseType.OK)
 
-    def get_chosen_category_value(self) -> str:
-        tree_iter = self.combobox.get_active_iter()
-        if tree_iter is not None:
-            model = self.combobox.get_model()
-            return model[tree_iter][0]
+    def _filter_func(self, model, p_iter, _):
+        text = self.get_chosen_category_value()
+        if text == "":
+            return True
 
-        return self.combobox.get_child().get_text()
+        return text in model[p_iter][0]
