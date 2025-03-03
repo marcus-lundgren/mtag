45de5caa9198ab07ba7473ba3a02a62b5120808e
Marcus Lundgren <marcus.lundgren@gmail.com>
2025-03-03 22:07:55 +0100

    Implemented more logic to the category details pane

--- mtag/web/www/static/js/category_page.js
+++ mtag/web/www/static/js/category_page.js
@@ -3,17 +3,25 @@ import { fetchCategories, fetchCategory } from "./api_client.js";
 const mainList = document.getElementById("main-list");
 const subList = document.getElementById("sub-list");
 const categoryNameInput = document.getElementById("category-name");
+const changeNameCheckbox = document.getElementById("change-name-checkbox");
 const categoryUrlInput = document.getElementById("category-url");
+const changeParentSelect = document.getElementById("change-parent-select");
+
+const updateCategoryDetails = async (categoryId) => {
+    const category = await fetchCategory(categoryId);
+    categoryNameInput.disabled = true;
+    categoryNameInput.value = category.name;
+    changeNameCheckbox.checked = false;
+    categoryUrlInput.value = category.url;
+    changeParentSelect.value = category.parent_id ?? -1;
+}
 
 const addSubOption = (sub) => {
     let option = document.createElement("option");
     option.text = sub.name;
     option.value = sub.db_id;
-    option.addEventListener("click", async () => {
-        const category = await fetchCategory(sub.db_id);
-        console.log(category);
-        categoryNameInput.value = category.name;
-        categoryUrlInput.value = category.url;
+    option.addEventListener("click", async (event) => {
+        await updateCategoryDetails(event.target.value);
     });
     subList.add(option);
 }
@@ -28,6 +36,14 @@ const addMainOption = (main, subs) => {
         // TODO - Fix this hack?
         main.name = "[Main]";
         addSubOption(main);
+
+        // Select the main from the subs list
+        subList.value = main.db_id;
+
+        // Update the details to show the main's information
+        updateCategoryDetails(main.db_id);
+
+        // Add the subs
         for (const sub of subs) {
             addSubOption(sub);
         }
@@ -35,14 +51,34 @@ const addMainOption = (main, subs) => {
     mainList.add(option);
 };
 
+const addParentChoiceOption = (name, categoryId) => {
+    let option = document.createElement("option");
+    option.text = name;
+    option.value = categoryId;
+    changeParentSelect.add(option);
+}
+
 const callFetchCategories = async () => {
     mainList.options.length = 0;
     subList.options.length = 0;
+    changeParentSelect.options.length = 0;
+
+    // Add an "empty" option
+    addParentChoiceOption("", -1);
 
     const json = await fetchCategories();
     for (const categoryTuple of json) {
-        addMainOption(categoryTuple.main, categoryTuple.children);
+        const main = categoryTuple.main;
+        addParentChoiceOption(main.name, main.db_id);
+        addMainOption(main, categoryTuple.children);
     }
 };
 
+const setupListeners = () => {
+    changeNameCheckbox.addEventListener("change", (event) => {
+        categoryNameInput.disabled = !changeNameCheckbox.checked;
+    });
+};
+
+setupListeners();
 callFetchCategories();
